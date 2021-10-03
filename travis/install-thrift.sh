bfa01525f93283d78c847757549fba398bd23eab
miyachu <79947589+miyachu@users.noreply.github.com>
2021-10-03 19:55:42 +0800

    Update install-thrift.sh

--- travis/install-thrift.sh
+++ travis/install-thrift.sh
@@ -9,12 +9,12 @@ set -e
 # Make it possible to get thrift in China
 # wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
 # tar -xzvf thrift-0.9.2.tar.gz
-git clone -b 0.9.2 https://github.com/apache/thrift.git thrift-0.9.2
+#git clone -b 0.9.2 https://github.com/apache/thrift.git thrift-0.9.2
 cd thrift-0.9.2
-./bootstrap.sh
-./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
-#make -j2 && sudo make install
-#cd lib/py
-#sudo python setup.py install
-#cd ../../..
-cd ../..
+#./bootstrap.sh
+#./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
+make -j2 && sudo make install
+cd lib/py
+sudo python setup.py install
+cd ../../..
+#cd ../..
